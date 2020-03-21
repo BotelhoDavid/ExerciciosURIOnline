1dc92ea71cd0a2501122023cf8379b3d2833dcfb
David Botelho <david.gbotelho@gmail.com>
2020-03-21 19:53:13 -0300

    Update 1046.cs
    Update

--- Iniciante/1046.cs
+++ Iniciante/1046.cs
@@ -16,35 +16,34 @@ class URI {
 
     static void Main(string[] args) {
 
-            int hora1, hora2;
-            DateTime date1, date2;
-            TimeSpan result;
+        int hora1, hora2;
+        DateTime date1, date2;
+        TimeSpan result;
 
-            var horas = Console.ReadLine().Split();
-            hora1 = int.Parse(horas[0]);
-            hora2 = int.Parse(horas[1]);
+        var horas = Console.ReadLine().Split();
+        hora1 = int.Parse(horas[0]);
+        hora2 = int.Parse(horas[1]);
 
-            date1 = new DateTime(1, 1, 1, hora1, 0, 0);
+        date1 = new DateTime(1, 1, 1, hora1, 0, 0);
 
-            if (hora2 <= hora1)
-            {
-                date2 = new DateTime(1, 1, 2, hora2, 0, 0);
-            }
-            else
-            {
-                date2 = new DateTime(1, 1, 1, hora2, 0, 0);
-            }
-
-            result = date2 - date1;
-
-            if (result.Days > 0)
-            {
-                Console.WriteLine($"O JOGO DUROU {(result.Days * 24) + result.Hours} HORA(S)");
-            }
-            else
-            {
-                Console.WriteLine($"O JOGO DUROU {result.Hours} HORA(S)");
-            }
+        if (hora2 <= hora1)
+        {
+            date2 = new DateTime(1, 1, 2, hora2, 0, 0);
+        }
+        else
+        {
+            date2 = new DateTime(1, 1, 1, hora2, 0, 0);
         }
 
+        result = date2 - date1;
+
+        if (result.Days > 0)
+        {
+            Console.WriteLine($"O JOGO DUROU {(result.Days * 24) + result.Hours} HORA(S)");
+        }
+        else
+        {
+            Console.WriteLine($"O JOGO DUROU {result.Hours} HORA(S)");
+        }
+    }
 }
